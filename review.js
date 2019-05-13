32acc1f6c4407083fbb3eb890d7e724014d038c0
ickeith <50379970+ickeith@users.noreply.github.com>
2019-05-13 15:03:18 -0500

    Add files via upload

---
+++ review.js
@@ -0,0 +1,51 @@
+console.log('GangGang')
+//string
+var sentence1 = 'cash rules everything around me'; 
+var sentence2 = 'C.R.E.A.M  get the money';
+var sentence3 = 'Dolla dolla bill yall';
+ 
+// numbers
+var jersey = 23;
+var block = 101;
+var age = 27;
+
+// boolean
+var Boom = true;
+var Pow = false;
+
+console.log(sentence2);
+console.log(jersey);
+console.log(Boom);
+
+//Equations
+var equations1 = 3*5;
+var equations2 = 20-5;
+var equations3 = 5*8;
+var equations4 = equations1 + 5;
+
+console.log(equations3);
+console.log(equations4);
+
+//Arrays
+var equationlist = [
+    equations1,
+    equations2,
+    equations3,
+    equations4
+];
+ var Rappers = ['Durk',
+ 'Reese',
+'Keef',
+'Herb'];
+
+var numbers = [20,10, 27,30];
+numbers.push(12);
+numbers.push(22);
+
+console.log(numbers);
+console.log(numbers.splice(0,2));
+Rappers.reverse();
+console.log(typeof jersey);
+var yourageis = `His age is ${age}`;
+
+
